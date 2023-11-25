fa7e125f567c1b398ade2179c61ed527ca59e092
actulurus <actulurus@gmail.com>
2023-11-25 19:51:44 +0100

    successfully downloads a book and saves it

--- main.py
+++ main.py
@@ -1,34 +1,54 @@
 import requests
-import json
+from bs4 import BeautifulSoup
+from urllib.parse import quote
 import os
 
-from bs4 import BeautifulSoup
+search_url = "https://www.eknihyzdarma.cz"
 
-urls = os.path.join(os.path.dirname(__file__), 'urls.json')
+def download_file(base_url, search_path, search_term, download_directory):
+    search_term_encoded = quote(search_term.encode('windows-1250'), safe='')
 
-with open(urls) as json_file:
-    urls = json.load(json_file)
-
-search_url = "https://www.eknihyzdarma.cz/eshop-fulltext-search.html?q=" # later we will have multiple websites so this will be included in the JSON
-
-def send_request_and_print_result(search_term):
-    url = f"{search_url}{search_term}"
-    print(url)
+    url = f"{base_url}{search_path}{search_term_encoded}"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     }
-    
+
     try:
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
+        response.raise_for_status()
 
-        print("Response from the server:")
-        
-        # write response.text to a file
-        with open('response.txt', 'w', encoding='utf-8') as f:
-            f.write(response.text)
+        found = True
+
+        if response.text.find("nepovedlo se vyhledat") != -1:
+            found = False
+
+        print(found)
+
+        if found:
+            # Parse the HTML content
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Find the download link using the specified class
+            download_link = soup.find('a', class_='download-book-link format-pdf')['href']
+            download_link = base_url + "/" + download_link
+
+            print(f"Download link: {download_link}")
+
+            # Download the file
+            file_response = requests.get(download_link)
+            file_response.raise_for_status()
+
+            # Get the filename from the URL
+            filename = os.path.join(download_directory, search_term + ".pdf")
+
+            # Save the file to the local directory
+            with open(filename, 'wb') as file:
+                file.write(file_response.content)
+
+            print(f"File downloaded to: {filename}")
+
     except requests.exceptions.RequestException as e:
-        print(f"Error sending the request: {e}")
+        print(f"Error: {e}")
 
-# Replace 'your_search_term_here' with the actual search term you want to use
-send_request_and_print_result('your_search_term_here')
+# Replace 'Babička' with your actual search term
+download_file(search_url, '/eshop-fulltext-search.html?q=', 'Babička', "C:\\Users\\actul\\Downloads\\DownloadedBooks")
