0baa54272cb46079089bdc8a4ed6725a7a5915a3
actulurus <actulurus@gmail.com>
2023-11-25 19:27:32 +0100

    first working demo/test to get HTML

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import requests
+import json
+import os
+
 from bs4 import BeautifulSoup
 
-def scrape_crypto_news(urls):
-    for url in urls:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+urls = os.path.join(os.path.dirname(__file__), 'urls.json')
 
-        # Extract relevant information from the webpage
-        # Modify this part based on the structure of the website
-        articles = soup.find_all('h2')
+with open(urls) as json_file:
+    urls = json.load(json_file)
 
-        for article in articles:
-            # Check if the article is related to Bitcoin
-            if 'bitcoin' in article.text.lower():
-                print(f"Website: {url}")
-                print(f"Title: {article.text}")
-                print("\n" + "=" * 50 + "\n")  # Separating articles for clarity
+search_url = "https://www.eknihyzdarma.cz/eshop-fulltext-search.html?q=" # later we will have multiple websites so this will be included in the JSON
 
-# Example URLs for crypto news
-crypto_news_urls = ['https://coindesk.com/', 'https://coinmarketcap.com/']
-scrape_crypto_news(crypto_news_urls)
+def send_request_and_print_result(search_term):
+    url = f"{search_url}{search_term}"
+    print(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    }
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+
+        print("Response from the server:")
+        
+        # write response.text to a file
+        with open('response.txt', 'w', encoding='utf-8') as f:
+            f.write(response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending the request: {e}")
+
+# Replace 'your_search_term_here' with the actual search term you want to use
+send_request_and_print_result('your_search_term_here')
