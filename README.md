443ac2a30cd3c772096ade537519c2381624573d
actulurus <actulurus@gmail.com>
2023-11-26 16:11:54 +0100

    added parameters/options, improved README.md

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 web scraper na maturitní četbu
 
 - stáhne všechny knihy jako .pdf
-- porovná knihy podle velikosi `(coming soon)`
-- seřadí knihy podle kategorií `(coming soon)`
+- seřadí knihy podle velikosi
+- vybere vám nejkratší knihy z každé kategorie.
 
 # Instalace
 - stáhni si repozitář `git clone https://github.com/actulurus/knihy-scraper.git`
@@ -11,4 +11,18 @@ web scraper na maturitní četbu
 - nainstaluj si knihovny `pip install -r requirements.txt`
 - (optional:) v `config.json` nastav download_dir na složku, kam chceš stahovat knihy (pokud neexistuje, vytvoří se) (default je `Downloads/DownloadedBooks`)
 - (optional:) pokud chceš stáhnout jen nějaké knihy, napiš je do `books.json` (formát musí zůstat stejný)
-- spusť `python main.py` a užívej četbu
+
+# Použití
+- spusť `python main.py` s parametrem `download` pro stažení knih
+- pokud chceš knihy seřadit podle délky, spusť `python main.py` s parametrami `sort`
+- pokud z nějakého důvodu nechceš debug logy, spusť `python main.py` s parametrem `silent`
+- pokud chceš automaticky smazat koruptované .pdf soubory, spusť `python main.py` s parametrem `deletecorrupted`
+
+# Disclaimer
+- občas se stane, že se nějakou knihu nepodaří stáhnout. ve většině případů je to způsobeno anti-botovskou ochranou, kterou má server, ze kterého se kniha stahuje. pokud opravdu moc chcete číst, je v takovém případě je potřeba knihu stáhnout ručně.
+- občas se také některá kniha stáhne, ale je poškozena. to fakt neni muj problém, pravděpodobně maj dosraný servery - můžete použít parametr `deletecorrupted`, který smaže všechny poškozené knihy.
+
+- jo a kdyžtak se tohle dá použít na jiný věci než knihy, ale musí to být PDF soubory.
+
+# Licence
+- dějej si s tim co chceš, ale nepudu za tebe do vězení za web scraping
--- main.py
+++ main.py
@@ -1,81 +1,105 @@
 import requests
 import os
+import sys
 import platform
 import json
 
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 from unidecode import unidecode
+import fitz  # PyMuPDF
 from colorama import init, Fore
 
 config = json.load(open('config.json', 'r', encoding='utf-8'))
 urls = json.load(open('urls.json', 'r', encoding='utf-8'))
 books = json.load(open('books.json', 'r', encoding='utf-8'))
 
+all_args = []
+
+for arg in sys.argv:
+    all_args.append(arg.lower())
+
 if platform.system() == "Linux" and config["download_dir"].startswith("C:"):
-    print(f"{Fore.RED}You're on Linux but the download directory starts with C: - please change it in config.json")
+    printfnc(f"{Fore.RED}You're on Linux but the download directory starts with C: - please change it in config.json")
     exit()
 
 if not os.path.exists(config["download_dir"]):
     os.makedirs(config["download_dir"])
 
 failed_books = []
+corrupted_books = []
+
+books_by_category = {}
+
+def printfnc(text):
+    if not "silent" in all_args:
+        print(text)
 
 def remove_special_characters(string):
     return "".join([c for c in string if c.isalpha() or c.isdigit() or c==' ']).rstrip()
+
+def count_pdf_pages(pdf_path):
+    try:
+        with fitz.open(pdf_path) as pdf_document:
+            return pdf_document.page_count
+    except Exception as e:
+        printfnc(f"Error counting pages: {e}")
+        return None
 
 def download_file(name=None, url=None, search_path=None, not_found_pattern=None, search_term=""):
     try:
         book_name = search_term.split(' - ')[1]
 
-        print(f"{Fore.LIGHTBLUE_EX}Searching for: {book_name}")
+        printfnc(f"{Fore.LIGHTBLUE_EX}Searching for: {book_name}")
     
         book_name = quote(book_name.encode('windows-1250'), safe='')
 
         filename = os.path.join(config["download_dir"], remove_special_characters(search_term) + ".pdf")
 
-        if os.path.exists(filename):
-            print(f"{Fore.YELLOW}File already exists")
-            return
+        if not os.path.exists(filename):
+            if "download" in all_args:
+                search_url = f"{url}{search_path}{book_name} pdf"
+                
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                }
 
-        search_url = f"{url}{search_path}{book_name} pdf"
-        
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        }
+            
+                response = requests.get(search_url, headers=headers)
+                response.raise_for_status()
 
-    
-        response = requests.get(search_url, headers=headers)
-        response.raise_for_status()
+                found = True
+                
+                if response.text.find(not_found_pattern) != -1:
+                    printfnc(f"{Fore.RED}Failed to find book - no search results")
+                    return
+                
+                soup = BeautifulSoup(response.text, 'html.parser')
 
-        found = True
-        
-        if response.text.find(not_found_pattern) != -1:
-            print(f"{Fore.RED}Failed to find book - no search results")
-            return
-        
-        soup = BeautifulSoup(response.text, 'html.parser')
+                download_link = soup.find('a', jsname="UWckNb")["href"]
+                
+                printfnc(f"{Fore.LIGHTBLUE_EX}Download link: {download_link}")
+                
+                file_response = requests.get(download_link)
+                file_response.raise_for_status()
+                
+                with open(filename, 'wb') as file:
+                    file.write(file_response.content)
 
-        download_link = soup.find('a', jsname="UWckNb")["href"]
+                printfnc(f"{Fore.GREEN}File downloaded to: {filename}")
+        else:
+            printfnc(f"{Fore.YELLOW}File already exists")
         
-        print(f"{Fore.LIGHTBLUE_EX}Download link: {download_link}")
-        
-        file_response = requests.get(download_link)
-        file_response.raise_for_status()
-        
-        with open(filename, 'wb') as file:
-            file.write(file_response.content)
-
-        print(f"{Fore.GREEN}File downloaded to: {filename}")
+        return filename
 
     except requests.exceptions.RequestException as e:
-        print(f"{Fore.RED}Error: {e}")
+        printfnc(f"{Fore.RED}Error: {e}")
 
         failed_books.append(search_term)
 
 def main():
     for url in urls:
-        print(f"{Fore.LIGHTBLUE_EX}Trying URL: " + url)
+        printfnc(f"{Fore.LIGHTBLUE_EX}Trying URL: " + url)
 
         data = urls[url]
 
@@ -84,7 +108,22 @@ def main():
 
         for category in books:
             for book in books[category]:
-                download_file(name=url, url=data["url"], search_path=data["search_path"], not_found_pattern=data["not_found_pattern"], search_term=book)
+                filename = download_file(name=url, url=data["url"], search_path=data["search_path"], not_found_pattern=data["not_found_pattern"], search_term=book)
+
+                if filename is not None:
+                    pages = count_pdf_pages(filename)
+
+                    if pages is not None:
+                        if category not in books_by_category:
+                            books_by_category[category] = {}
+                    
+                        books_by_category[category][book] = pages
+                    elif "download" in all_args:
+                        corrupted_books.append(book)
+
+                        if "deletecorrupted" in all_args:
+                            printfnc(f"{Fore.RED}Deleting corrupted file: {filename}")
+                            os.remove(filename)
             
 if __name__ == "__main__":
     main()
@@ -94,12 +133,31 @@ if __name__ == "__main__":
     i = 0
     for book in failed_books:
         i += 1
-
         message += book + (", " if i < len(failed_books) else "")
 
-    print(message)
+    if len(corrupted_books) > 0:
+        message += f"{Fore.RED} | Corrupted books: "
+
+        i = 0
+        for book in corrupted_books:
+            i += 1
+            message += book + (", " if i < len(corrupted_books) else "")
+
+    printfnc(message)
 
     if len(failed_books) > 0:
-        print(Fore.MAGENTA + "FIY: If any books failed to download, it's probably caused by the website having policies against web scraping.")
+        printfnc(Fore.MAGENTA + "FYI: If any books failed to download, it's probably caused by the website having policies against web scraping. If files are corrupted, the program will detect and optionally delete them. The reason for files being corrupted is unknown, most likely an issue on the server.")
     
+    printfnc(Fore.CYAN + "Books by length:")
+
+    for category, books_in_category in books_by_category.items():
+        if "sort" in all_args:
+            books_in_category = sorted(books_in_category.items(), key=lambda x: x[1] if x[1] is not None else float('inf'))
+
+        print(f"{Fore.LIGHTBLUE_EX}{category}:")
+        for book, page_count in (books_in_category if "sort" in all_args else books_in_category.items()):
+            if page_count is not None:
+                print(f"{Fore.CYAN}{book}:   {page_count} pages")
+
+
     print(Fore.WHITE)
